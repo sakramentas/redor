b879fd721e6b787f70e3f628adc288181583b0f0
Lucas Sacramento <lucas.arki@gmail.com>
2017-10-15 20:22:12 +0100

    Created Endpoints constants file

---
+++ src/api/endpoints.js
@@ -0,0 +1,2 @@
+export const TICKETMASTER_ENDPOINT = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=TLAdwV0eyURqxMPWSG8lnw9IvLH37GEZ&city=dublin&size=50&sort=date,name,asc';
+export const EVENTBRITE_ENDPOINT = 'https://www.eventbriteapi.com/v3/events/search/?location.address=dublin&sort_by=date&token=SV7XRDVTKSTYYJOV4NU4';
