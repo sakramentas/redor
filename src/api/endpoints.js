1c841ff2aa8db34e9e2856daca74ea69fd99128e
Lucas Sacramento <lucas.arki@gmail.com>
2017-10-30 17:50:19 +0000

    Created build GmapsAnchorurl

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -6,6 +6,7 @@ export const GMAPS_STATIC_API_KEY = 'AIzaSyAotqkbCfXbmlfRYx43Ph_yq9rhN4Qo2Jw';
 
 const TICKETMASTER_URL = 'https://app.ticketmaster.com/discovery/v2';
 const EVENTBRITE_URL = 'https://www.eventbriteapi.com/v3';
+const GMAPS_ANCHOR_URL = 'https://www.google.com/maps/search/';
 const GMAPS_STATIC_URL = 'https://maps.googleapis.com/maps/api/staticmap';
 
 const DEFAULT_CITY = 'dublin';
@@ -14,6 +15,12 @@ export const buildGmapsStaticImageUrl = (lat, lon) => {
   const params = `center=${lat},${lon}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${lon}&key=${GMAPS_STATIC_API_KEY}`;
 
   return `${GMAPS_STATIC_URL}?${params}`;
+};
+
+export const buildGmapsAnchorUrl = (lat, lon) => {
+  const params = `api=1&query=${lat},${lon}`;
+console.log("`${GMAPS_ANCHOR_URL}?${params}`", `${GMAPS_ANCHOR_URL}?${params}`);
+  return `${GMAPS_ANCHOR_URL}?${params}`;
 };
 
 // --- Endpoint builders for Ticketmaster ---
@@ -39,7 +46,7 @@ export const buildFetchEventsEventbriteEndpoint = (sort = 'date', category = 'ev
   return `${EVENTBRITE_URL}/${category}/search?${params}`;
 };
 
-export const buildFetchVenueEventbriteEndpoint = (id, category = 'venues') => {
+export const buildFetchCommonEventbriteEndpoint = (id, category) => {
   const params = `&token=${EVENTBRITE_API_KEY}`;
 
   return `${EVENTBRITE_URL}/${category}/${id}?${params}`;
