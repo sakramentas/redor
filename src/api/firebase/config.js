18c94d22a3efdfea032f9f035acfdd2607be3e1f
Lucas Sacramento <lucas.arki@gmail.com>
2017-11-28 18:53:12 +0000

    Prepared Firebase environment

---
+++ src/api/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBynJIT2v0VV3QeuWYdd7pvCbAbVdW82hs',
+  authDomain: 'dublin-events-1509214523643.firebaseapp.com',
+  databaseURL: 'https://dublin-events-1509214523643.firebaseio.com',
+  projectId: 'dublin-events-1509214523643',
+  storageBucket: 'dublin-events-1509214523643.appspot.com',
+  messagingSenderId: '893744536833',
+};
+
+export default firebaseConfig;
