a43529ba3787e4dc6ac5c8640ed8cdec9a82bb5e
Lucas Sacramento <lucas.arki@gmail.com>
2018-05-14 22:45:30 +0100

    Added new Date helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
 import moment from 'moment';
 
 export const getDateNowFull = () => `${moment().format('YYYY-MM-DDTHH:mm:ss')}Z`;
+export const transformDateFull = dateTime => `${moment(dateTime).format('dddd, h:mm a')}`;
+export const transformTimefull = dateTime => `${moment(dateTime).format('LT')}`;
